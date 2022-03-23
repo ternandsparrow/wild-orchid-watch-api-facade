c58a929b1e443f9c8029315eb3c582f67857a0fd
Tom Saleeba <tom.saleeba@gmail.com>
2022-03-23 13:47:24 -0600

    tidy up

--- .circleci/config-env.sh
+++ .circleci/config-env.sh
@@ -1,11 +1,13 @@
 # intended to be sourced
-echo ${GCP_PROJECT_KEY} | base64 --decode \
-  --ignore-garbage > $HOME/gcloud-service-key.json
-echo 'export GOOGLE_CLOUD_KEYS=$(cat $HOME/gcloud-service-key.json)' >> $BASH_ENV
+set -euxo pipefail
+keyFile=$HOME/gcloud-service-key.json
+echo ${GCP_PROJECT_KEY} | base64 --decode --ignore-garbage > $keyFile
+echo "export GOOGLE_CLOUD_KEYS=$(cat $keyFile)" >> $BASH_ENV
 IMAGE_NAME=$CIRCLE_PROJECT_REPONAME
-echo 'export IMAGE_NAME=$IMAGE_NAME' >> $BASH_ENV
+echo "export IMAGE_NAME=$IMAGE_NAME" >> $BASH_ENV
 export IMAGE_PREFIX=gcr.io/${GOOGLE_PROJECT_ID:?}/$IMAGE_NAME
 export IMAGE_TAG=${CIRCLE_SHA1:?}
 IMAGE_FULL=$IMAGE_PREFIX:$IMAGE_TAG
-echo 'export IMAGE_FULL=$IMAGE_FULL' >> $BASH_ENV
+echo "export IMAGE_FULL=$IMAGE_FULL" >> $BASH_ENV
+cat $BASH_ENV
 source $BASH_ENV

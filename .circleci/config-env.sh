6909c4318e8148289f852d4e00cb84ccc9e58408
Tom Saleeba <tom.saleeba@gmail.com>
2022-03-23 15:14:03 -0600

    set project id

--- .circleci/config-env.sh
+++ .circleci/config-env.sh
@@ -2,12 +2,17 @@
 set -euxo pipefail
 keyFile=$HOME/gcloud-service-key.json
 echo ${GCP_PROJECT_KEY:?} | base64 --decode --ignore-garbage > $keyFile
+
+# these lines are for where we directly use gcloud
 gcloud auth activate-service-account --key-file $keyFile
+gcloud config set project ${GOOGLE_PROJECT_ID:?}
+
 # this var used by gcp-gcr/gcr-auth step
 echo "export GOOGLE_CLOUD_KEYS=\$(cat $keyFile)" >> $BASH_ENV
+
 IMAGE_NAME=${CIRCLE_PROJECT_REPONAME:?}
 echo "export IMAGE_NAME=$IMAGE_NAME" >> $BASH_ENV
-export IMAGE_PREFIX=gcr.io/${GOOGLE_PROJECT_ID:?}/$IMAGE_NAME
+export IMAGE_PREFIX=gcr.io/$GOOGLE_PROJECT_ID/$IMAGE_NAME
 export IMAGE_TAG=${CIRCLE_SHA1:?}
 echo "export IMAGE_FULL=$IMAGE_PREFIX:$IMAGE_TAG" >> $BASH_ENV
 cat $BASH_ENV
